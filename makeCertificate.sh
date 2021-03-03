761b8e49613528cf0ef779a8d6965b1f4ff39dd8
Frosty Labs <35430845+FrostyLabs@users.noreply.github.com>
2021-03-03 23:40:38 +0100

    Update makeCertificate.sh

--- makeCertificate.sh
+++ makeCertificate.sh
@@ -3,19 +3,19 @@
 if [ "$#" -ne 2 ]
 then
   /usr/bin/echo "Usage: must supply a domain, and IP address"
-  /usr/bin/echo "Example: ./makeCertificate.sh your.domain.local 192.168.1.10
+  /usr/bin/echo "Example: bash makeCertificate.sh your.domain.local 192.168.1.10"
   exit 1
 fi
 
 DOMAIN=$1
 IP=$2
 
-mkdir -p ~/ca/$DOMAIN
-cd ~/ca/$DOMAIN
+/usr/bin/mkdir -p ./ca/$DOMAIN
+cd ./ca/$DOMAIN
 
 /usr/bin/openssl genrsa -out $DOMAIN.key 4096
 
-cat >$DOMAIN.conf <<EOF
+cat >$DOMAIN.conf<<EOF
 [req]
 prompt = no
 default_bits = 4096
@@ -23,12 +23,11 @@ default_md = sha512
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltNames = @alt_names
 distinguished_name = dn
-basicConstraints = CA:FALSE
 
 [dn]
-C=CH
-ST=ZH
-L=ZH
+C=US
+ST=NY
+L=NY
 O=Frosty Labs
 CN=$DOMAIN
 
@@ -37,25 +36,25 @@ DNS.1=$DOMAIN
 IP=$IP
 EOF
 
-cat >$DOMAIN.ext <<EOF
+cat >$DOMAIN.ext<<EOF
 authorityKeyIdentifier = keyid,issuer
 basicConstraints=CA:FALSE
+subjectKeyIdentifier = hash
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 
 [alt_names]
 DNS.1=$DOMAIN
 IP=$IP
-
 EOF
 
 /usr/bin/openssl req -new -key $DOMAIN.key -out $DOMAIN.csr -config $DOMAIN.conf
 
 /usr/bin/openssl x509 -req -in $DOMAIN.csr \
-  -CA /opt/ca/root/root.pem \
-  -CAkey /opt/ca/root/root.key \
+  -CA ./ca/root/root.pem \
+  -CAkey ./ca/root/root.key \
   -CAcreateserial \
-  -out $DOMAIN.crt -days 365 -sha512 \
-  -extfile $DOMAIN.ext
-
+  -extfile $DOMAIN.ext \
+  -out $DOMAIN.crt -days 365 -sha512
+  
 /usr/bin/echo "[*] Done"
