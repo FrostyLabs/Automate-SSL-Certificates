6e4ce7d4db496a7d67a14c8ef2296a99f84a22df
UnRunDead <me@unrundead.dev>
2021-03-04 10:07:36 +0100

    Improved scripts and edited readme

--- makeCertificate.sh
+++ makeCertificate.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]
-then
-  /usr/bin/echo "Usage: must supply a domain, and IP address"
-  /usr/bin/echo "Example: bash makeCertificate.sh your.domain.local 192.168.1.10"
-  exit 1
-fi
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -d|-domain|--domain)
+      domain="$2"
+      ;;
+    -i|-ip|--ip)
+      ip="$2"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+  shift
+done
 
-DOMAIN=$1
-IP=$2
-CWD=$(/usr/bin/pwd)
+# Update these variables
+# Path where the files should be created
+yourPath=/your/path/of/choice
+# Path to your Root Certificate
+rootPath=/path/of/root/cert
 
-/usr/bin/mkdir -p ./ca/$DOMAIN
-cd ./ca/$DOMAIN
+/usr/bin/mkdir -p $yourPath/$domain
+cd $yourPath/$domain
 
-/usr/bin/openssl genrsa -out $DOMAIN.key 4096
+/usr/bin/openssl genrsa -out $domain.key 4096
 
-cat >$DOMAIN.conf<<EOF
+cat >$domain.conf<<EOF
 [req]
 prompt = no
 default_bits = 4096
@@ -25,19 +38,20 @@ keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltNames = @alt_names
 distinguished_name = dn
 
+# Update these parameters
 [dn]
 C=US
 ST=NY
 L=NY
-O=Frosty Labs
-CN=$DOMAIN
+O=YourCoolName
+CN=$domain
 
 [alt_names]
-DNS.1=$DOMAIN
-IP=$IP
+DNS.1=$domain
+IP=$ip
 EOF
 
-cat >$DOMAIN.ext<<EOF
+cat >$domain.ext<<EOF
 authorityKeyIdentifier = keyid,issuer
 basicConstraints=CA:FALSE
 subjectKeyIdentifier = hash
@@ -45,17 +59,17 @@ keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 
 [alt_names]
-DNS.1=$DOMAIN
-IP=$IP
+DNS.1=$domain
+IP=$ip
 EOF
 
-/usr/bin/openssl req -new -key $DOMAIN.key -out $DOMAIN.csr -config $DOMAIN.conf
+/usr/bin/openssl req -new -key $domain.key -out $domain.csr -config $domain.conf
 
-/usr/bin/openssl x509 -req -in $DOMAIN.csr \
-  -CA $CWD/ca/root/root.pem \
-  -CAkey $CWD/ca/root/root.key \
+/usr/bin/openssl x509 -req -in $domain.csr \
+  -CA $rootPath/root.pem \
+  -CAkey $rootPath/root.key \
   -CAcreateserial \
-  -extfile $DOMAIN.ext \
-  -out $DOMAIN.crt -days 365 -sha512
+  -extfile $domain.ext \
+  -out $domain.crt -days 365 -sha512
   
 /usr/bin/echo "[*] Done"
